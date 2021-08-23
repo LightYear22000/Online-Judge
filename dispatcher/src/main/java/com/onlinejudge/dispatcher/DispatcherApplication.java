83d5bb6f4c46c676067755207bd41b8075417103
Prem Bhawnani <prembhawnani.pb1412@gmail.com>
2021-08-23 23:41:34 +0530

    init spring project

---
+++ dispatcher/src/main/java/com/onlinejudge/dispatcher/DispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.onlinejudge.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherApplication.class, args);
+    }
+
+}
---
+++ dispatcher/src/test/java/com/onlinejudge/dispatcher/DispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinejudge.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
