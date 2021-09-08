a831d6393171601402fba922d773e10858ff1408
Sameer Raj <sameerraj2k@gmail.com>
2021-09-08 22:03:01 +0530

    init java sandbox

---
+++ JavaSandbox/src/main/java/com/onlinejudge/javasandbox/JavaSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.onlinejudge.javasandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSandboxApplication.class, args);
+    }
+
+}
---
+++ JavaSandbox/src/test/java/com/onlinejudge/javasandbox/JavaSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinejudge.javasandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
